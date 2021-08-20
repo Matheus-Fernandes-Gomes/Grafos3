be7decab8b9afa46b70e7846b28539da64551146
Matheus Fernandes <matheusfernandesgomes1@gmail.com>
2021-08-20 02:20:01 -0300

    Alterações e criação da função first

--- algoritimo_construtivo.py
+++ algoritimo_construtivo.py
@@ -1,3 +1,4 @@
+from os import SEEK_CUR
 import numpy as np
 import timeit
 class algoritimo_construtivo:
@@ -5,8 +6,12 @@ class algoritimo_construtivo:
     def __init__(self, arquivo):
         # Classe com todos os atributos e metodos do algoritimo construtivo
         #self.morigem,self.mdestino,self.menor = self.menor(arquivo)
+        #cria lista
+        self.lista= []
+        self.n_vertices=0
         self.lista,self.resu = self.constroi(arquivo)
         self.imprimi(self.lista,self.resu)
+        #self.menor_no_vertice(arquivo,3)
 
     def imprimi(self,Lista,resu):
         print(resu)
@@ -15,35 +20,44 @@ class algoritimo_construtivo:
     def constroi(self, arquivo):
         
         #lendo o cabeçalho
-        header = arquivo.readline()
+        header = arquivo.readline().rstrip()  
+        self.n_vertices=int(header[0])
         info = header.split(' ')
         
-        #cria lista
-        lista = []
         #percorre cada linha do arquivo
         #atribui infinito para menor 
         menor=float("inf")
         resu=0
         #partindo do menor
         vertice1,destino1,tamanho1=self.menor(arquivo)
-        print("ponnto1")
-        print(vertice1,destino1,tamanho1)
-        lista.append(vertice1)
-        morigem,mdestino1,menor=self.menor_no_vertice(arquivo,vertice1)
-        print("ponto 2")
-        print(vertice1,destino1,menor)
-        lista.append(morigem)
-        morigem1,mdestino,menor=self.menor_no_vertice(arquivo,mdestino1)
-        print("ponto3")
-        print(vertice1,destino1,menor)
-        lista.append(morigem1)
+        self.primeiro=vertice1
+        resu=resu+tamanho1
+        #print("ponnto1")
+        #print(vertice1,destino1,tamanho1)
+        self.lista.append(vertice1)
+        
+        while(len(self.lista)!=(self.n_vertices+1)):
+            self.lista.append(destino1)
+            m_destino,menor=self.menor_no_vertice(arquivo,destino1)
+            #print("teste")
+            #print(m_destino,menor)
+            destino1=m_destino
+            resu=resu+menor 
 
+        '''
+        i=0
+        for i in range(5):
+            print('i=',i)
+            morigem,mdestino,menor=self.menor_no_vertice(arquivo,i)
+            print("origem:   ",morigem,mdestino,menor)
+            self.lista.append(mdestino)
             
 
 
+       
 
         
-        '''for header in arquivo:
+        for header in arquivo:
             info = header.split(' ')
             origem = int(info[0])
             #testa para cada linha do vertice
@@ -51,7 +65,7 @@ class algoritimo_construtivo:
                 morigem,mdestino,menor=self.menor_no_vertice(arquivo,origem)
                 resu=resu+menor
                 lista.append(morigem)'''
-        return(lista,resu)
+        return(self.lista,resu)
     
 
     def menor(self,arquivo):
@@ -74,15 +88,16 @@ class algoritimo_construtivo:
                 morigem=int(info[0])
                 mdestino=int(info[1])
         #imprimi teste
-        print("ponto 4")
-        print(morigem,mdestino,menor)
+        #print("ponto 4")
+        #print(morigem,mdestino,menor)
         return (morigem,mdestino,menor)
 
 
     def menor_no_vertice(self,arquivo,vertice):
+        #coloca o cursor no inicio do arquivo para
+        arquivo.seek(0)
         #atribuindo infinito a menor p
         menor=float("inf")
-        morigem=0
         mdestino=0
         #lendo o cabeçalho
         header = arquivo.readline()
@@ -94,13 +109,56 @@ class algoritimo_construtivo:
             origem = int(info[0])
             destino = int(info[1])
             peso = float(info[2])
-            #verifica se os vertices são iguais
-            if(vertice == origem):
-                #enontra o menor 
-                if peso<menor:
-                    menor=peso
-                    morigem=origem
-                    mdestino=destino
-            print("***************")
-            print(morigem,mdestino,menor)
-        return (morigem,mdestino,menor)
+            #verifica se o vertice está na listada
+            if(destino not in self.lista):
+                if(vertice == origem):
+                    #enontra o menor 
+                    if peso<menor:
+                        menor=peso
+                        
+                        mdestino=destino
+            elif(origem not in self.lista):
+                if(vertice == destino):
+                    if peso<menor:
+                        menor=peso
+                        mdestino=origem
+
+
+            #print("***************")
+            #print(morigem,mdestino,menor)
+        if (menor ==float("inf")):
+            (mdestino,menor)=self.first(arquivo,vertice)
+                
+        return (mdestino,menor)
+
+    def first(self,arquivo,mdestino):
+         #coloca o cursor no inicio do arquivo para
+        arquivo.seek(0)
+        #atribuindo infinito a menor 
+        mdestino=0
+        #lendo o cabeçalho
+        header = arquivo.readline()
+        info = header.split(' ')
+        #primeiro na listada
+        primeiro=self.lista[0]
+        
+        #acha menor
+        for header in arquivo:
+            info = header.split(' ')
+            #salvando o numero de vertices e arestas
+            origem = int(info[0])
+            destino = int(info[1])
+            peso = float(info[2])
+            if(origem==mdestino):
+                if(destino==primeiro):
+                    #print("Primeiro na listada",peso)
+                    return (primeiro,peso)
+                    
+
+            if(destino==mdestino):
+                if(origem==primeiro):
+                    #print("Primeiro na listada",peso)
+                    return (primeiro,peso)
+                    
+                
+
